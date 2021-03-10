d0bc6807aeb2a092fcb6878ecff3cc608ca34b33
Alex <barankaslon@hotmail.com>
2021-03-09 23:15:16 -0400

    initial commit

---
+++ monsters/src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import CardList from './components/card-list/card-list'
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+    render() {
+      return (
+        <div className="App">
+          <input
+            type='search'
+            placeholder='search monsters'
+            onChange={e=> 
+              this.setState({ searchField: e.target.value })
+            } />
+          <CardList monsters={this.state.monsters} />
+        </div>
+      );
+    }
+}
+
+export default App;
