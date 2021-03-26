53030df0ab0d8dc3fb963c9c7a1f8577c0df5cf3
Alex <barankaslon@hotmail.com>
2021-03-26 02:30:21 -0300

    React connected

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,54 +1,18 @@
-import { createStore, bindActionCreators } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import reduser from './reducer';
-import * as actions from './actions';
+import App from './components/app';
 
 
 
 const store = createStore(reduser);
 
-const {dispatch} = store;
 
-/* const bindActionCreator = (creator, dispatch) => (...args) => {
-  dispatch(creator(...args));
-} */
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+, document.getElementById('root'));
 
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch)
-/* const decDispatch = bindActionCreators(dec, dispatch)
-const rndDispatch = bindActionCreators(rnd, dispatch) */
-
-document.getElementById('inc').addEventListener('click', inc)
-
-document.getElementById('dec').addEventListener('click', dec)
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value)
-})
-
-const update = () => {
-  document.getElementById('counter').textContent = store.getState();
-}
-
-
-store.subscribe(update);
-
-
-/* store.subscribe(() => {
-  console.log(store.getState());
-})
-
-
-
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'}); */
-
-
-
-
-
-/* let state = reduser(undefined, {});
-
-state = reduser(state, {type: 'INC'});
-console.log(state);
-state = reduser(state, {type: 'INC'});
-console.log(state); */
