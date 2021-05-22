9cda62861b9862f403cab42d25ce9b3c8c9842a5
Alex <barankaslon@hotmail.com>
2021-05-22 13:34:14 -0300

    style works fith product component

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -11,13 +11,17 @@ function Product() {
                 $1499
             </Price>
             <Rating>
-
+                :star:
             </Rating>
 
-            <Image />
-            <AddToCartButton>
-                Add to Cart
-            </AddToCartButton>
+            <Image src="https://www.bhphotovideo.com/images/images2500x2500/apple_my232ll_a_11_ipad_pro_early_1553824.jpg"/>
+
+            <ActionSection>
+                <AddToCartButton>
+                    Add to Cart
+                </AddToCartButton>
+            </ActionSection>
+
 
 
         </Container>
@@ -33,20 +37,35 @@ const Container = styled.div`
     padding: 20px;
     margin: 10px;
     max-height: 400px;
+    display: flex;
+    flex-direction: column;
 `
 
 const Title = styled.span`
 
 `
 const Price = styled.span`
-
+    font-weight: 500;
+    margin-top: 3px;
 `
 const Rating = styled.div`
 
 `
 const Image = styled.img`
-
+    max-height: 200px;
+    object-fit: contain;
 `
 const AddToCartButton = styled.button`
+    width: 100px;
+    height: 30px;
+    background-color: #f0c14b;
+    border: 2px solid #a88734;
+    border-radius: 2px;
+    margin: 10px;
+`
 
+const ActionSection = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
