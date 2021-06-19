78a8b65398e0bb9973f04904d2e3e01d0b27984f
Alex <barankaslon@hotmail.com>
2021-06-18 21:42:11 -0300

    initial commit

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -30,7 +30,6 @@ const Banner = styled.div `
     mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0))
 `
 const Content = styled.div `
-    background: #fff;
     padding: 0 10px;
     margin-top: -350px;
     display: flex;
