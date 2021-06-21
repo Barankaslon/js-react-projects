e595c7a7d6e795ae513dfe5cceb5417171c5975d
Alex <barankaslon@hotmail.com>
2021-06-20 22:21:05 -0300

    home page

--- tiktok-clone/src/index.js
+++ tiktok-clone/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+import Header from './components/Header'
+
+import './App.css';
+
+
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Header />
+      <Switch>
+        <Route path='/upload' component={Upload}/>
+        <Route path='/' component={Home}/>
+      </Switch>
+    </HashRouter>
+  );
+}
+
+export default App;
 
 ReactDOM.render(
   <React.StrictMode>
