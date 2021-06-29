b9742ee351d907b21211673297c849d1c3470287
Alex <barankaslon@hotmail.com>
2021-06-28 22:39:09 -0300

    setting netlify

--- tiktok-clone/functions/addData.js
+++ tiktok-clone/functions/addData.js
@@ -1,9 +1,18 @@
 const { createClient } = require("@astrajs/collections");
 
+const collection = 'posts'
 
-const astraClient = await createClient({
-    astraDatabaseId: process.env.ASTRA_DB_ID,
-    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-    username: process.env.ASTRA_DB_USERNAME,
-    password: process.env.ASTRA_DB_PASSWORD,
-});
+exports.handler = async function (event, context, callback) {
+    const astraClient = await createClient({
+        astraDatabaseId: process.env.ASTRA_DB_ID,
+        astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+        username: process.env.ASTRA_DB_USERNAME,
+        password: process.env.ASTRA_DB_PASSWORD,
+    });
+
+    const posts = astraClient
+        .namespace(process.env.ASTRA_DB_KEYSPACE)
+        .collection(collection)
+}
+
+
